47d1d981fb0b91f07710f9ff8e083f5426aaacf5
adiaz <adiaz@106-04.info2>
2014-05-28 09:40:43 +0200

    Cambios hechos desde eclipse para web

--- prueba/src/complejo/TestComplejo.java
+++ prueba/src/complejo/TestComplejo.java
@@ -1,25 +1,30 @@
 package complejo;
 
-// Esto es un test para la clase Complejo.
-public class TestComplejo
-{
-	public static void main(String[] args)
-	{
+// Este archivo ha sido modificado desde el espacio de trabajo workspaceWeb,
+// tras importar el proyecto desde la web de github
+
+// Esto es un test para la clase Complejo
+public class TestComplejo {
+	public static void main(String[] args) {
 		Complejo com1 = new Complejo();
-		Complejo com2 = new Complejo(1.4,2);
-		
-		System.out.println("Otra forma de escribirlo es "+com1.toString());
+		Complejo com2 = new Complejo(1.4, 2);
+
+		System.out.println("Otra forma de escribirlo es " + com1.toString());
 		com1.setMod(5.4);
 		com1.setArg(-3);
-		System.out.println("Tengo un numero complejo de modulo "+com1.getMod()+" y argumento "+com1.getArg());
-		System.out.println("Otra forma de escribirlo es "+com1.toString());
-		
-		System.out.print("Tengo otro numero complejo "+com2.toString());
-		System.out.print(" y al multiplicarlo por el primero tenemos otro complejo, que es ");
+		System.out.println("Tengo un numero complejo de modulo "
+				+ com1.getMod() + " y argumento " + com1.getArg());
+		System.out.println("Otra forma de escribirlo es " + com1.toString());
+
+		System.out.print("Tengo otro numero complejo " + com2.toString());
+		System.out
+				.print(" y al multiplicarlo por el primero tenemos otro complejo, que es ");
 		System.out.println(com1.calculaProducto(com2));
-		System.out.print("Si dividimos el primero entre el segundo, el resultado es ");
+		System.out
+				.print("Si dividimos el primero entre el segundo, el resultado es ");
 		System.out.println(com1.calculaDivision(com2));
-		System.out.print("Si elevamos "+com1.toString()+" a 3, el resultado es ");
+		System.out.print("Si elevamos " + com1.toString()
+				+ " a 3, el resultado es ");
 		System.out.println(com1.calculaPotencia(3));
 	}
 }
